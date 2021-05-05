8de77938206972b55556cebc62b8f629d9eb48d9
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-05-05 15:56:00 -0300

    Add files via upload

--- regex.py
+++ regex.py
@@ -5,7 +5,7 @@ email2 = "Fale comigo em 1234-1234 esse é meu telefone"
 email3 = "1234-1234 é o meu celular"
 email4 = "lalalalala 3216-6547 haoehaeahuashuash"
 
-padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"
+padrao = "[0-9]{4}[-][0-9]{4}"
 
 retorno = re.search(padrao, email4)
 print(retorno.group())
